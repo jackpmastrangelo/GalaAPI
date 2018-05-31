82edb604b942be7de52137b9c3ed6d6606f76fbe
Jack Mastrangelo <jack@Jacks-MacBook-Pro.local>
2018-05-31 13:32:41 -0400

    Initial Spring Application

---
+++ src/main/java/Gala/galaapi/GalaApiApplication.java
@@ -0,0 +1,12 @@
+package Gala.galaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaApiApplication.class, args);
+	}
+}
