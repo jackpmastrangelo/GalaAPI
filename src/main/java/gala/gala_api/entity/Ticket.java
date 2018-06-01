455ea3447507ec8d6bce3234733697d4d04c05c3
Jack Mastrangelo <jack@Jacks-MacBook-Pro.local>
2018-06-01 08:42:51 -0400

    Ticket Entity first pass and JavaDoc

--- src/main/java/gala/gala_api/entity/Ticket.java
+++ src/main/java/gala/gala_api/entity/Ticket.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,24 +17,68 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.lang.NonNull;
 
+/**
+ * This class represents a Ticket enitity in the Gala API.
+ */
 @Entity
 @Table (name = "Ticket")
 @EntityListeners(AuditingEntityListener.class)
 @JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
 public class Ticket implements Serializable {
     
+    /**
+     * A Ticket's unique Id.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NonNull
     private Long id;
 
+    /**
+     * The Id of the event associated with this Ticket.
+     */
     @NonNull
     private Long eventId;
 
+    /**
+     * The email associated with the creation and sending of this Ticket.
+     */
     @NotBlank
     private String email;
 
+    /**
+     * The status of this ticket.
+     */
     @NotBlank
+    @Enumerated(EnumType.STRING)
     private TicketStatus status;
+
+    /**
+     * Get the unique Id of a Ticket.
+     */
+    public Long getId() {
+        return this.id;
+    }
+
+    /**
+     * Get the id for the event associated with this Ticket.
+     */
+    public Long getEventId() {
+        return this.eventId;
+    }
+
+    /**
+     * Get the email associated with this Ticket.
+     */
+    public String getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Get this Ticket's status.
+     */
+    public TicketStatus getStatus() {
+        return this.status;
+    }
 
 }
--- src/main/java/gala/gala_api/entity/TicketStatus.java
+++ src/main/java/gala/gala_api/entity/TicketStatus.java
@@ -1,12 +1,19 @@
 package gala.gala_api.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-@Entity
-@Table()
+/**
+ * This enumeration represents the possible statuses of a ticket.
+ */
 public enum TicketStatus {
-    ACTIVE,
+    /**
+     * Ticket has been created, granted and not validated. Still able to be validated.
+     */
+    ACTIVE, 
+    /**
+     * Ticket is unable to be used but has not been validated (ie. Event is in past)
+     */
     INACTIVE,
-    VALIDATED;
+    /**
+     * Ticket has been validated at event and cannot be validated again.
+     */
+    VALIDATED; 
 }
