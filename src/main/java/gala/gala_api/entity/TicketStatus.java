f82600741c7d8814aed4ac0f3e171b57634bb498
Daniel Goldstein <danielgold95@gmail.com>
2018-06-04 21:31:00 -0400

    Clean up and fixed the entity mapping

--- src/main/java/gala/gala_api/entity/TicketStatus.java
+++ src/main/java/gala/gala_api/entity/TicketStatus.java
@@ -7,13 +7,13 @@ public enum TicketStatus {
     /**
      * Ticket has been created, granted and not validated. Still able to be validated.
      */
-    ACTIVE, 
+    ACTIVE,
     /**
-     * Ticket is unable to be used but has not been validated (ie. Event is in past)
+     * Ticket is unable to be used but has not been validated (i.e. Event is in past)
      */
     INACTIVE,
     /**
      * Ticket has been validated at event and cannot be validated again.
      */
-    VALIDATED; 
+    VALIDATED
 }
