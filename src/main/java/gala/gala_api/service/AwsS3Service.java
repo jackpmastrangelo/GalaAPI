458335a04de221f65b3451ff6e920c0d26c047b6
Jack Mastrangelo <mastrangelo.j@husky.neu.edu>
2018-07-01 15:38:20 -0400

    Consolidation of the S3 client

--- src/main/java/gala/gala_api/service/AwsS3Service.java
+++ src/main/java/gala/gala_api/service/AwsS3Service.java
@@ -1,14 +1,21 @@
 package gala.gala_api.service;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -20,7 +27,15 @@ import java.io.InputStreamReader;
 @Service
 public class AwsS3Service {
 
-  private AmazonClient amazonClient;
+  private AmazonS3 amazonClient;
+
+  @Value("${amazonProperties.accessKey}")
+  private String accessKey;
+
+  @Value("${amazonProperties.secretKey}")
+  private String secretKey;
+
+  private final Regions region = Regions.US_EAST_1;
 
   /**
    * Gets the given s3 file and returns its contents as a large string. Currently does not preserver newlines.
@@ -31,7 +46,7 @@ public class AwsS3Service {
    * @return The s3 file's contents as a String.
    */
   public String getS3ObjectAsString(String bucket, String key) {
-    AmazonS3 client = this.amazonClient.getAmazonClient();
+    AmazonS3 client = this.amazonClient;
     S3Object emailTemplateObj = client.getObject(bucket, key);
     BufferedReader s3Reader = new BufferedReader(new InputStreamReader(emailTemplateObj.getObjectContent()));
 
@@ -49,14 +64,22 @@ public class AwsS3Service {
   }
 
   public void putS3ObjectFromByteArray(byte[] s3Obj, String bucket, String key) {
-    AmazonS3 client = this.amazonClient.getAmazonClient();
+    AmazonS3 client = this.amazonClient;
     ByteArrayInputStream inputStream = new ByteArrayInputStream(s3Obj);
     PutObjectRequest request = new PutObjectRequest(bucket, key, inputStream, new ObjectMetadata());
     client.putObject(request);
   }
 
-  @Autowired
-  public void setAmazonClient(AmazonClient amazonClient) {
+  @PostConstruct
+  private void initClient() {
+    AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+    AWSCredentialsProvider provider = new AWSStaticCredentialsProvider(credentials);
+    this.amazonClient = AmazonS3ClientBuilder.standard().withCredentials(provider)
+            .withRegion(region).build();
+  }
+
+  //For testing purposes.
+  public void setAmazonClient(AmazonS3 amazonClient) {
     this.amazonClient = amazonClient;
   }
 }
